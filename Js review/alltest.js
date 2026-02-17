aa717bf2766a8dab0a0fca9c66957cc00d1f45a7
Mupenzi Herve <mupenziherve04@gmail.com>
2026-02-17 22:38:01 +0200

    one day to go

--- Js review/alltest.js
+++ Js review/alltest.js
@@ -5,3 +5,27 @@ function evenOrOdd() {
     return 'Odd'
   }
 }
+
+function evenOrOdd() {
+  if (20 % 2 === 0) {
+    return 'Even'
+  } else {
+    return 'Odd'
+  }
+}
+
+function evenOrOdd() {
+  if (20 % 2 === 0) {
+    return 'Even'
+  } else {
+    return 'Odd'
+  }
+}
+function evenOrOdd() {
+  if (20 % 2 === 0) {
+    return 'Even'
+  } else {
+    return 'Odd'
+  }
+}
+ 
