339613c8ed9cd8319eea9441fb8d00df55203c73
Mupenzi Herve <mupenziherve04@gmail.com>
2026-02-19 23:52:39 +0200

    simple calculate

--- Js review/alltest.js
+++ Js review/alltest.js
@@ -1,2 +1,9 @@
+function addStringInputs(str1, str2) {
+  const num1 = Number(str1)
+  const num2 = Number(str2)
+  return num1 + num2
+}
 
- 
+
+const result = addStringInputs('5', '10')
+console.log(result)
