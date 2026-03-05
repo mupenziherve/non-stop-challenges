05fadf0866a938508beb55e887203c5d2234344f
Mupenzi Herve <mupenziherve04@gmail.com>
2026-03-05 21:22:08 +0200

    3d card import

--- nextjs-typescript/next-application/app/layout.tsx
+++ nextjs-typescript/next-application/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
+import Footer from './components/Footer'
 
 export const metadata: Metadata = {
   title: 'next-application',
@@ -13,7 +14,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" data-theme="night">
-      <body>{children}</body>
+      <body className="min-h-screen flex flex-col">
+        <main className="flex-grow">{children}</main>
+
+        <Footer />
+      </body>
     </html>
   )
 }
--- nextjs-typescript/next-application/app/page.tsx
+++ nextjs-typescript/next-application/app/page.tsx
@@ -5,42 +5,25 @@ import ProductCard from './components/ProductCard'
 export default function Home() {
   return (
     <main>
-      <footer className="footer sm:footer-horizontal bg-base-200 text-base-content p-10">
-        <nav>
-          <h6 className="footer-title">Services</h6>
-          <a className="link link-hover">Branding</a>
-          <a className="link link-hover">Design</a>
-          <a className="link link-hover">Marketing</a>
-          <a className="link link-hover">Advertisement</a>
-        </nav>
-        <nav>
-          <h6 className="footer-title">Company</h6>
-          <a className="link link-hover">About us</a>
-          <a className="link link-hover">Contact</a>
-          <a className="link link-hover">Jobs</a>
-          <a className="link link-hover">Press kit</a>
-        </nav>
-        <nav>
-          <h6 className="footer-title">Legal</h6>
-          <a className="link link-hover">Terms of use</a>
-          <a className="link link-hover">Privacy policy</a>
-          <a className="link link-hover">Cookie policy</a>
-        </nav>
-        <form>
-          <h6 className="footer-title">Newsletter</h6>
-          <fieldset className="w-80">
-            <label>Enter your email address</label>
-            <div className="join">
-              <input
-                type="text"
-                placeholder="username@site.com"
-                className="input input-bordered join-item"
-              />
-              <button className="btn btn-primary join-item">Subscribe</button>
-            </div>
-          </fieldset>
-        </form>
-      </footer>
+      <div className="hover-3d">
+        {/* content */}
+        <figure className="max-w-100 rounded-2xl">
+          <img
+            src="https://img.daisyui.com/images/stock/creditcard.webp"
+            alt="3D card"
+          />
+        </figure>
+        {/* 8 empty divs needed for the 3D effect */}
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+
       <Link href="/users">Users</Link>
       <ProductCard />
     </main>
