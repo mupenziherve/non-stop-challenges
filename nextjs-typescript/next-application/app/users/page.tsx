9eeadb10ed3d01880740e18066bfddf79dd11c35
Mupenzi Herve <mupenziherve04@gmail.com>
2026-03-05 14:02:19 +0200

    debug on name to title

--- nextjs-typescript/next-application/app/users/page.tsx
+++ nextjs-typescript/next-application/app/users/page.tsx
@@ -3,18 +3,18 @@ import React from "react";
 interface User {
 
   id: number;
-  name: string;
+  title: string;
 }
 
 const usersPage = async () => {
-const res = await fetch('https://jsonplaceholder.typicode.com/users/1/posts')
+const res = await fetch('https://jsonplaceholder.typicode.com/users/1/posts');
 const users: User[] = await res.json();
 
   return (
     <>
     <h1>Users</h1>
     <ul>
-      {users.map(user => user.id)}
+      {users.map(user => <li key={user.id}>{user.title}</li> )}
     </ul>
     </>
   );
