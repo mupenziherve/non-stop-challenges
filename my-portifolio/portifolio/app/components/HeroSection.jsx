2327babee51395c3da70abe53d21b80f18e8a880
Mupenzi Herve <mupenziherve04@gmail.com>
2026-03-06 20:46:02 +0200

    HeroSection

--- my-portifolio/portifolio/app/components/HeroSection.jsx
+++ my-portifolio/portifolio/app/components/HeroSection.jsx
@@ -9,19 +9,32 @@ const HeroSection = () => {
             Hello, I'm Herve
           </h1>
 
-          <p className="text-[#adb7be] text-lg lg:text-xl">
+          <p className="text-[#adb7be] mb-6 text-lg lg:text-xl">
             Writing code to solve real-world problems and build meaningful
             digital solutions.
           </p>
+          <div>
+            <button className="bg-white rounded-full text-black px-6 py-3 mr-4 hover:bg-slate-200">
+              Hire Me
+            </button>
+            <button className="px-6 py-3 rounded-full hover:bg-green text-white border-white mt-3">
+              Download CV
+            </button>
+          </div>
         </div>
 
-        <div className="col-span-5 flex justify-center">
-          <div className="rounded-full overflow-hidden w-[250px] h-[250px]">
+        <div className="col-span-5 flex justify-center mt-8 lg:mt-0">
+          <div
+            className="rounded-full overflow-hidden 
+              w-50 h-50 
+              sm:w-62.5 sm:h-62.5
+               lg:w-87.5 lg:h-87.5"
+          >
             <Image
               src="/passport.png"
               alt="hero image"
-              width={250}
-              height={250}
+              width={350}
+              height={350}
               className="object-cover"
             />
           </div>
