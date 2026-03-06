19419ba9a362a099ba02561fc18e115566450b45
Mupenzi Herve <mupenziherve04@gmail.com>
2026-03-07 00:52:14 +0200

    redesining

--- my-portifolio/portifolio/app/components/HeroSection.jsx
+++ my-portifolio/portifolio/app/components/HeroSection.jsx
@@ -3,22 +3,26 @@ import Image from 'next/image'
 const HeroSection = () => {
   return (
     <section>
-      <div className="grid grid-cols-1 lg:grid-cols-12 items-center">
-        <div className="col-span-7 place-self-center">
-          <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
-            Hello, I'm Herve
+      <div className="grid grid-cols-1 sm:grid-cols-12 ">
+        <div className="col-span-7 place-self-center text-center sm:text-left">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+            <span class="text-transparent bg-clip-text bg-linear-to-r from-[#6480ff] via-[#3e9bba] to-[#2fa3a1]">
+              Hello, I'm{''}
+            </span>
+            Herve
           </h1>
 
-          <p className="text-[#adb7be] mb-6 text-lg lg:text-xl">
+          <p className="text-[#adb7be] text-base mb-6 sm:text-lg lg:text-xl">
             Writing code to solve real-world problems and build meaningful
             digital solutions.
           </p>
           <div>
-            <button className="bg-white rounded-full text-black px-6 py-3 mr-4 hover:bg-slate-200">
+            <button className="bg-linear-to-r from-[#6480ff] via-[#3e9bba] to-[#2fa3a1]  w-full sm:w-fit rounded-full text-white px-6 py-3 mr-4 hover:bg-slate-200">
               Hire Me
             </button>
-            <button className="px-6 py-3 rounded-full hover:bg-green text-white border-white mt-3">
-              Download CV
+            <button className="px-1 py-1 w-full sm:w-fit  rounded-full bg-linear-to-br from-[#6480ff] via-[#3e9bba] to-[#2fa3a1] hover:bg-slate-800 text-white ">
+              <div className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-3 ">
+                Download CV</div>
             </button>
           </div>
         </div>
