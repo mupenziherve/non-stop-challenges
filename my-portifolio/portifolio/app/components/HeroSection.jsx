822820babad1b8e9bb0a8f02754f938bf0b44409
Mupenzi Herve <mupenziherve04@gmail.com>
2026-03-07 13:05:31 +0200

    NavbarSection

--- my-portifolio/portifolio/app/components/HeroSection.jsx
+++ my-portifolio/portifolio/app/components/HeroSection.jsx
@@ -1,4 +1,6 @@
+'use client'
 import Image from 'next/image'
+import { TypeAnimation } from 'react-type-animation'
 
 const HeroSection = () => {
   return (
@@ -6,10 +8,26 @@ const HeroSection = () => {
       <div className="grid grid-cols-1 sm:grid-cols-12 ">
         <div className="col-span-7 place-self-center text-center sm:text-left">
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
-            <span class="text-transparent bg-clip-text bg-linear-to-r from-[#6480ff] via-[#3e9bba] to-[#2fa3a1]">
+            <span className="text-transparent bg-clip-text bg-linear-to-r from-[#6480ff] via-[#3e9bba] to-[#2fa3a1]">
               Hello, I'm{''}
             </span>
-            Herve
+            <br></br>
+            <TypeAnimation
+              sequence={[
+                'Herve',
+                1000,
+                'Front-end Developer ',
+                1000,
+                'Back-end Developer',
+                1000,
+                'UI/UX Designer',
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              
+              repeat={Infinity}
+            />
           </h1>
 
           <p className="text-[#adb7be] text-base mb-6 sm:text-lg lg:text-xl">
@@ -20,9 +38,10 @@ const HeroSection = () => {
             <button className="bg-linear-to-r from-[#6480ff] via-[#3e9bba] to-[#2fa3a1]  w-full sm:w-fit rounded-full text-white px-6 py-3 mr-4 hover:bg-slate-200">
               Hire Me
             </button>
-            <button className="px-1 py-1 w-full sm:w-fit  rounded-full bg-linear-to-br from-[#6480ff] via-[#3e9bba] to-[#2fa3a1] hover:bg-slate-800 text-white ">
+            <button className="px-1 py-1 w-full sm:w-fit  rounded-full bg-linear-to-br from-[#6480ff] via-[#3e9bba] to-[#2fa3a1] hover:bg-slate-800 text-white mt-3 ">
               <div className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-3 ">
-                Download CV</div>
+                Download CV
+              </div>
             </button>
           </div>
         </div>
