95bb89021ed6135c0850b3c48685c8adbb5cc111
Mupenzi Herve <mupenziherve04@gmail.com>
2026-03-08 22:54:31 +0200

    custom logo

--- my-portifolio/portifolio/app/components/Navbar.jsx
+++ my-portifolio/portifolio/app/components/Navbar.jsx
@@ -1,22 +1,25 @@
-import Link from "next/link";
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <nav>
-      <div className="flex flex-wrap items-center justify-between mx-auto p-8">
-        <Link href="/" className="text-5xl text-white font-semibold">
-            MH
+    <nav className="w-full px-12 py-6">
+      <div className="flex items-center justify-between">
+       
+        <Link href="/" className="text-4xl text-white font-bold">
+          {'<MH>'}
         </Link>
-        <div className="menu hidden md:block md:w-auto" id="navbar" >
-            <ul>
-                <li><Link href={"#about"} className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white">About</Link>  </li>
-            </ul>
 
-        </div>
 
+        <ul className="flex gap-8">
+          <li>
+            <Link href="#about" className="text-[#ADB7BE] hover:text-white">
+              About
+            </Link>
+          </li>
+        </ul>
       </div>
     </nav>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
--- my-portifolio/portifolio/app/page.tsx
+++ my-portifolio/portifolio/app/page.tsx
@@ -1,17 +1,18 @@
-import Image from 'next/image'
+import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
-import Navbar from "./components/Navbar"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212]">
-      <div className="container mx-auto px-12 py-4">
-        <Navbar/>
-        <HeroSection />
+    <main className="min-h-screen bg-[#0a0a0a]">
 
-        
+      {/* Navbar */}
+      <Navbar />
+
+      {/* Space between navbar and hero */}
+      <div className="mt-16 px-12">
+        <HeroSection />
       </div>
-      
+
     </main>
-  )
+  );
 }
