7a931d5f6820908af9b47ac465ce1e89c4049493
Mupenzi Herve <mupenziherve04@gmail.com>
2026-03-08 12:41:45 +0200

    too many bugs

--- my-portifolio/portifolio/app/page.tsx
+++ my-portifolio/portifolio/app/page.tsx
@@ -1,12 +1,17 @@
 import Image from 'next/image'
 import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
       <div className="container mx-auto px-12 py-4">
+        <Navbar/>
         <HeroSection />
+
+        
       </div>
+      
     </main>
   )
 }
