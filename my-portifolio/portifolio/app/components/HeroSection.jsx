636df4261b2167f8de40566ca1fcde5140f28e55
Mupenzi Herve <mupenziherve04@gmail.com>
2026-03-08 22:57:01 +0200

    custom logo

--- my-portifolio/portifolio/app/components/HeroSection.jsx
+++ my-portifolio/portifolio/app/components/HeroSection.jsx
@@ -4,7 +4,7 @@ import { TypeAnimation } from 'react-type-animation'
 
 const HeroSection = () => {
   return (
-    <section>
+    <section>ddddddddddddddddddddddddddddddddddddddd
       <div className="grid grid-cols-1 sm:grid-cols-12 ">
         <div className="col-span-7 place-self-center text-center sm:text-left">
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
--- my-portifolio/portifolio/app/components/Navbar.jsx
+++ my-portifolio/portifolio/app/components/Navbar.jsx
@@ -8,6 +8,16 @@ const Navbar = () => {
         <Link href="/" className="text-4xl text-white font-bold">
           {'<MH>'}
         </Link>
+        import Link from 'next/link'
+
+const Navbar = () => {
+  return (
+    <nav className="w-full px-12 py-6">
+      <div className="flex items-center justify-between">
+       
+        <Link href="/" className="text-4xl text-white font-bold">
+          {'<MH>'}
+        </Link>
 
 
         <ul className="flex gap-8">
