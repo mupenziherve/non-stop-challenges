5f5e24b6acd4310a4634a03ef8eea9edff55b915
Mupenzi Herve <mupenziherve04@gmail.com>
2026-03-09 18:36:23 +0200

    toggle workingn

--- my-portifolio/portifolio/app/components/Navbar.jsx
+++ my-portifolio/portifolio/app/components/Navbar.jsx
@@ -1,39 +1,44 @@
+'use client'
+
 import Link from 'next/link'
 import NavLink from './NavLink'
-import { title } from 'node:process'
-import path from 'node:path'
+import React, { useState } from 'react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
 
-const navLinks =[
-  {
-    title: "About",
-    path: "#about",
-  },
-  {
-    title: "Projects",
-    path: "#projects",
-  },
-  {
-    title: "Contact",
-    path: "#contact"
-  }
+const navLinks = [
+  { title: 'About', path: '#about' },
+  { title: 'Projects', path: '#projects' },
+  { title: 'Contact', path: '#contact' },
 ]
 
 const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false)
+
   return (
     <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212]/90">
-      <div className="flex flex-wrap items-center justify-between mx-auto px-7 p-5">
+      <div className="flex flex-wrap items-center justify-between mx-auto p-5 py-2">
         <Link href="/" className="text-3xl md:text-4xl text-cyan-500 font-bold">
           {'<MH>'}
         </Link>
-        <div className="menu block md:w-auto" id="navbar">
+
+        <button
+          onClick={() => setNavbarOpen(!navbarOpen)}
+          className="flex items-center px-3 py-2 border rounded text-slate-200 hover:text-white hover:border-white md:hidden"
+        >
+          {navbarOpen ? (
+            <XMarkIcon className="h-5 w-5" />
+          ) : (
+            <Bars3Icon className="h-5 w-5" />
+          )}
+        </button>
+
+        <div className="menu hidden md:block md:w-auto">
           <ul className="flex gap-8 p-4 md:flex-row md:space-x-8 mt-0">
-            {navLinks.map((link, index) => {
-              return (
-                <li key={index}>
-                  <NavLink href={link.path} title={link.title} />
-                </li>
-              )
-            })}
+            {navLinks.map((link, index) => (
+              <li key={index}>
+                <NavLink href={link.path} title={link.title} />
+              </li>
+            ))}
           </ul>
         </div>
       </div>
