e03df406b9afe99e322eb76b2a325eb1e83fa7d2
Mupenzi Herve <mupenziherve04@gmail.com>
2026-03-09 17:35:51 +0200

    mobo-hambager-icon

--- my-portifolio/portifolio/app/components/Navbar.jsx
+++ my-portifolio/portifolio/app/components/Navbar.jsx
@@ -1,23 +1,41 @@
 import Link from 'next/link'
 import NavLink from './NavLink'
+import { title } from 'node:process'
+import path from 'node:path'
+
+const navLinks =[
+  {
+    title: "About",
+    path: "#about",
+  },
+  {
+    title: "Projects",
+    path: "#projects",
+  },
+  {
+    title: "Contact",
+    path: "#contact"
+  }
+]
 
 const Navbar = () => {
   return (
-    <nav className="w-full px-12 py-6">
-      <div className="flex items-center justify-between">
-       
-        <Link href="/" className="text-4xl text-white font-bold">
+    <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212]/90">
+      <div className="flex flex-wrap items-center justify-between mx-auto px-7 p-5">
+        <Link href="/" className="text-3xl md:text-4xl text-cyan-500 font-bold">
           {'<MH>'}
         </Link>
-
-          
-        <ul className="flex gap-8">
-          <li>
-            <Link href="#about" className="text-[#ADB7BE] hover:text-white">
-              About
-            </Link>
-          </li>
-        </ul>
+        <div className="menu block md:w-auto" id="navbar">
+          <ul className="flex gap-8 p-4 md:flex-row md:space-x-8 mt-0">
+            {navLinks.map((link, index) => {
+              return (
+                <li key={index}>
+                  <NavLink href={link.path} title={link.title} />
+                </li>
+              )
+            })}
+          </ul>
+        </div>
       </div>
     </nav>
   )
--- my-portifolio/portifolio/app/page.tsx
+++ my-portifolio/portifolio/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-[#0a0a0a]">
  <Navbar />
-      <div className="mt-16 px-12">
+      <div className="container mt-40 mx-auto ">
         <HeroSection />
       </div>
 
