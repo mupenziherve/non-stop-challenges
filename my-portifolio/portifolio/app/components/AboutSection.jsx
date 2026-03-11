e451057bfddfb19661a079112d8634dc26594e45
Mupenzi Herve <mupenziherve04@gmail.com>
2026-03-11 14:07:26 +0200

    move on certification

--- my-portifolio/portifolio/app/components/AboutSection.jsx
+++ my-portifolio/portifolio/app/components/AboutSection.jsx
@@ -1,11 +1,23 @@
 import Image from "next/image";
 
 const AboutSection = () => {
-  return <section  className="text-white"><div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-    
-    
-    
-    </div>AboutUs
-    </section>;
+  return (
+    <section className="text-white">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/done.png" alt="About-image" width={500} height={500} />
+        <div>
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            I am a full stack web developer with a passion for creating
+            interactive and responsive web applications. I have experience
+            working with JavaScript, React, Redux, Node.js, Express, PostgreSQL,
+            TypeScript,Tailwind,Figma, HTML, CSS, and Git. I am a quick learner and I am always
+            looking to expand my knowledge and skill set. I am a team player and
+            I am excited to work with others to create amazing applications.
+          </p>
+        </div>
+      </div>
+    </section>
+  )
 };
 export default AboutSection;
