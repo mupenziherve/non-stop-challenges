09af925fe63614f7c702cc15441da5f8fa907775
Mupenzi Herve <mupenziherve04@gmail.com>
2026-03-12 11:36:50 +0200

    tab Button

--- my-portifolio/portifolio/app/components/TabButton.jsx
+++ my-portifolio/portifolio/app/components/TabButton.jsx
@@ -5,7 +5,8 @@ const TabButton = ({ active, selectTab,children }) => {
       ? 'text-white  border-b border-b-cyan-500'
       : 'text-[#ADB7BE]'
   return (
-    <button on>
+    <button onClick={selectTab}>
+      <p className="mr-3 font-semibold hover:text-white  "></p>
       {children}
     </button>
   )
