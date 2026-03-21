4586038e35164901a40234122c59311263bda693
Mupenzi Herve <mupenziherve04@gmail.com>
2026-03-21 23:38:53 +0200

    understand process

--- my-portifolio/portifolio/app/components/ProjectCard.jsx
+++ my-portifolio/portifolio/app/components/ProjectCard.jsx
@@ -1,14 +1,14 @@
 
 const ProjectCard = ({ imgUrl, title, description }) => {
   return (
-    <div className="rounded-lg overflow-hidden shadow-lg">
+    <div>
       <div
-        className="h-52 md:h-72 bg-cover bg-center"
-        style={{ backgroundImage: `url(${imgUrl})` }}
+        className="h-52 md:h-72 bg-cover rounded-t-xl"
+        style={{ backgroundImage: `url(${imgUrl})` ,backgroundSize: "cover" }}
       ></div>
       <div className="p-4 text-white bg-gray-800">
-        <h5 className="text-lg font-semibold mb-2">{title}</h5>
-        <p className="text-sm">{description}</p>
+        <h5 className="text-xl font-semibold mb-2">{title}</h5>
+        <p className="text-[#ADB7BE]">{description}</p>
       </div>
     </div>
   )
