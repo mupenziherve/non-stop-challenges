15670ef95a695c334e7454069fd150bc03d3cffc
Mupenzi Herve <mupenziherve04@gmail.com>
2026-03-24 00:33:33 +0200

    LucidIcon

--- my-portifolio/portifolio/app/components/EmailSection.jsx
+++ my-portifolio/portifolio/app/components/EmailSection.jsx
@@ -1,6 +1,18 @@
+
+
 const EmailSection = () => {
-  return <section className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4">
-    <div><h5 className="text-xl font-bold text-white my-2">Let's Connect</h5></div>
-  </section>;
+  return (
+    <section className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-3">
+      <div>
+        <h5 className="text-xl font-bold text-white my-2">Let's Connect</h5>
+        <p className="text-[#ADB7BE]">
+          I’m currently open to new opportunities where I can help design and
+          develop scalable, high-quality digital products. Whether you have a
+          project, an idea, or just want to connect, feel free to reach out.
+        </p>
+        <div className="socials flex flex-row gap-2"></div>
+      </div>
+    </section>
+  )
 };
 export default EmailSection;
