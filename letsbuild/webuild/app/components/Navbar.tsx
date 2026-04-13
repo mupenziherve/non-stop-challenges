012284fff66cbef0d6d77c7fa4ab97652a20f23a
Mupenzi Herve <mupenziherve04@gmail.com>
2026-04-13 22:07:07 +0200

    cnu

--- letsbuild/webuild/app/components/Navbar.tsx
+++ letsbuild/webuild/app/components/Navbar.tsx
@@ -1,5 +1,11 @@
-export default function Navbar () {
 
-    return 
-
-}
+export const Navbar = () => {
+  return (
+    <nav className="flex justify-between p-6 bg- shadow">
+      <h2 className="">Webuild</h2>
+      <a href="#">Home</a>
+      <a href="#">About</a>
+      <a href="#">Contacts</a>
+    </nav>
+  )
+};
